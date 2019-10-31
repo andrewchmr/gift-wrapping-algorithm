f7b2f9295d98e1a29148a4ff9d7bd0db776902d0
Andrzej Chemerinskiy <andrzej.chemerinskiy@blue-veery.com>
2019-10-31 16:21:48 +0100

    Updates

---
+++ precache-manifest.061f6fa477205b55aa7465cd844588b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd02bbde599831a3c6f4a52ea7a1618f",
+    "url": "/gift-wrapping-algorithm/index.html"
+  },
+  {
+    "revision": "fda5b9f9b5a17147fee9",
+    "url": "/gift-wrapping-algorithm/static/css/main.b981f001.chunk.css"
+  },
+  {
+    "revision": "cb23e3c8a494853b355a",
+    "url": "/gift-wrapping-algorithm/static/js/2.7bb62946.chunk.js"
+  },
+  {
+    "revision": "fda5b9f9b5a17147fee9",
+    "url": "/gift-wrapping-algorithm/static/js/main.6b99cb3d.chunk.js"
+  },
+  {
+    "revision": "1a04aca1bb09350aa432",
+    "url": "/gift-wrapping-algorithm/static/js/runtime-main.394f400f.js"
+  }
+]);
